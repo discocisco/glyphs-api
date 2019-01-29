6d2e0625afbf7181a23526283bbbe644f91a8425
discocisco <f.andresort@gmail.com>
2019-01-29 14:19:51 -0500

    Add favorite scaffold NEW
    - Create favorites table
    - Migrate create favorites file
    - Modify models to reflect relationships for fonts and users

--- app/models/font.rb
+++ app/models/font.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Font < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :favorites, except: %i[new edit]
   resources :fonts, except: %i[new edit]
 
   # RESTful routes
