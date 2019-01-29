e6f46dfcae16c22155bedebab90fb773f6e7812d
discocisco <f.andresort@gmail.com>
2019-01-29 17:06:46 -0500

    Add curl-scripts for fonts
    - Add or change create, index, show, update, and destroy

--- curl-scripts/fonts/update.sh
+++ curl-scripts/fonts/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl "http://localhost:4741/fonts" \
+curl "http://localhost:4741/fonts/${ID}" \
   --include \
-  --request POST \
+  --request PATCH \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
